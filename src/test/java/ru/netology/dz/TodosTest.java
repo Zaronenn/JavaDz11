40acca4c197708738efdb4537f6a99d83beb7881
Sergey Medvedev <msergejev@mail.ru>
2023-12-08 20:18:16 +0300

    search is completed

--- src/test/java/ru/netology/dz/TodosTest.java
+++ src/test/java/ru/netology/dz/TodosTest.java
@@ -49,4 +49,33 @@ public class TodosTest {
 
         Assertions.assertArrayEquals(expected, actual);
     }
+    @Test
+    public void shouldReturnEmptyArrayWhenSearchingWithNoMatches() {
+        Todos todos = new Todos();
+        todos.add(new SimpleTask(1, "Купить молоко"));
+        todos.add(new Epic(2, new String[]{"Сделать домашнее задание"}));
+        Task[] expected = {};
+        Task[] actual = todos.search("Погладить кота");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnArrayOfTasksWhenSearchingWithOneMatch() {
+        Todos todos = new Todos();
+        todos.add(new SimpleTask(1, "Купить молоко"));
+        todos.add(new Epic(2, new String[]{"Сделать домашнее задание"}));
+        Task[] expected = { new SimpleTask(1, "Купить молоко") };
+        Task[] actual = todos.search("молоко");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnArrayOfAllTasksWhenSearchingWithEmptyQuery() {
+        Todos todos = new Todos();
+        todos.add(new SimpleTask(1, "Купить молоко"));
+        todos.add(new Epic(2, new String[]{"Сделать домашнее задание"}));
+        Task[] expected = { new SimpleTask(1, "Купить молоко"), new Epic(2, new String[]{"Сделать домашнее задание"}) };
+        Task[] actual = todos.search("");
+        Assertions.assertArrayEquals(expected, actual);
+    }
 }
